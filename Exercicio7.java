7f402c0a5df63f11d710b2e3f254389d9e68a381
pliniooop <pedroservulo980@gmail.com>
2026-05-05 21:42:53 -0300

    Add files via upload

---
+++ Exercicio7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+
+    // Método que calcula o fatorial
+    public static long calcularFatorial(int n) {
+        long fatorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+
+        return fatorial;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int numero = scanner.nextInt();
+
+        long resultado = calcularFatorial(numero);
+
+        System.out.println("Fatorial de " + numero + " = " + resultado);
+
+        scanner.close();
+    }
+}
