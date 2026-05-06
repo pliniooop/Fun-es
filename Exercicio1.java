5051e4d7094852e4086fc15709f7b46007be761e
pliniooop <pedroservulo980@gmail.com>
2026-05-05 21:26:32 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,15 @@
+public class Exercicio1 {
+
+    // Função sem retorno
+    public static void mensagem() {
+        System.out.println("Meu primeiro programa com funcoes");
+    }
+
+    public static void main(String[] args) {
+
+        // Chamando a função três vezes
+        mensagem();
+        mensagem();
+        mensagem();
+    }
+}
